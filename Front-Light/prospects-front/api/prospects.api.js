238be8bcab1961fa17c5d1b0262b4714291b7aa7
Brayan <programador.brayan@calzzapato.com>
2024-06-14 18:17:13 -0700

    Se añade api al front

---
+++ Front-Light/prospects-front/api/prospects.api.js
@@ -0,0 +1,35 @@
+import { api } from 'api/request'
+
+const BASE_URL = '/prospect'
+
+export const prospect = {
+    getProspect: async () =>{
+        return await api({
+            method: 'GET',
+            endpoint: `${BASE_URL}`,
+        })
+    },
+    getDocuments: async (idProspect) =>{
+        return await api({
+            method: 'GET',
+            enpoint: `${BASE_URL}/documents?idProspect=${idProspect}`
+        })
+    },
+    createProspect: async (data) =>{
+        return await api({
+            method: 'POST',
+            endpoint: `${BASE_URL}/create`,
+            data
+        })
+    },
+    updateStatus: async (data) =>{
+        return await api({
+            method: 'PATCH',
+            endpoint: `${BASE_URL}/prospect-status`,
+            data
+        })
+    },
+    
+}
+
+export default prospectApi
